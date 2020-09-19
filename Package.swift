e4165e191789f84a65e6f6779be7262b428f61ac
Andrew Wagner <845683+drewag@users.noreply.github.com>
2020-09-19 13:32:41 -0600

    [UP] Update to Decree 4.3

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "DecreeServices",
     platforms: [
-        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9),
+        .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v4)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["DecreeServices"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/drewag/Decree.git", from: "4.3.0"),
+        .package(url: "https://github.com/drewag/Decree.git", from: "4.4.0"),
         .package(url: "https://github.com/drewag/CryptoSwift.git", from: "1.0.0"),
     ],
     targets: [
