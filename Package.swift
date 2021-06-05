4f9a0879830c54d97080026a2c94d26bda880ac3
Andrew Wagner <845683+drewag@users.noreply.github.com>
2021-06-04 23:57:42 -0600

    [CI] Fix build for Xcode 12

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "DecreeServices",
     platforms: [
-        .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v4)
+        .macOS(.v10_12), .iOS(.v9), .tvOS(.v9), .watchOS(.v4)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/drewag/Decree.git", from: "4.4.0"),
-        .package(url: "https://github.com/drewag/CryptoSwift.git", from: "1.0.0"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
